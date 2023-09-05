05e69b4d8f69596dc136cc49e688a9ecdb50b9d2
Sandhya <thaku.sandhya@gmail.com>
2023-09-05 13:13:08 +0530

    Updated React code

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -69,10 +69,6 @@ const Hero = () => {
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
         {/* gradient end */}
       </div>
-
-      <div className={`ss:hidden ${styles.flexCenter}`}>
-        <GetStarted />
-      </div>
     </section>
   );
 };
